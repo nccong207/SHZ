e111e9f89c49db0ce8b204fac44ccfc9900d1c5c
linh0893 <linh0893@gmail.com>
2017-04-02 23:39:39 +0700

    update UI for select 'chi nhánh'

--- TinhLuongGVCT/frmThang.cs
+++ TinhLuongGVCT/frmThang.cs
@@ -24,28 +24,38 @@ namespace TinhLuongGVCT
         Database db = Database.NewDataDatabase();
         public int iThang = 0;
         public int iNam = 0;
+        public string iChiNhanh = "";
         private void frmThang_Load(object sender, EventArgs e)
         {
             if (Config.GetValue("KyKeToan") != null)
                 spinThang.EditValue = Config.GetValue("KyKeToan");
             else
                 spinThang.EditValue = DateTime.Today.Month;
+
+            string sql = @"SELECT * FROM DMBoPhan";
+            DataTable dt = db.GetDataTable(sql);
+
+            cbChiNhanh.DataSource = dt;
+            cbChiNhanh.DisplayMember = "TenBP";
+            cbChiNhanh.ValueMember = "MaBP";
+
         }
 
         private void btnTinhLuong_Click(object sender, EventArgs e)
         {
             int nam = DateTime.Today.Year;
             iThang = int.Parse(spinThang.EditValue.ToString());
+            iChiNhanh = cbChiNhanh.SelectedValue.ToString();
             iNam = Int32.Parse(Config.GetValue("NamLamViec").ToString());
             if (Config.GetValue("NamLamViec") != null)
                 nam = Int32.Parse(Config.GetValue("NamLamViec").ToString());
-            _gvDetail.ActiveFilterString = "Thang = '" + spinThang.EditValue.ToString() + "' and Nam = '" + nam.ToString() + "'";
+            _gvDetail.ActiveFilterString = "Thang = '" + spinThang.EditValue.ToString() + "' and Nam = '" + nam.ToString() + "' and MaCN = '" + cbChiNhanh.SelectedValue.ToString() + "'";
             if (_gvDetail.DataRowCount > 0)
             {
                 this.Close();
                 return;
             }
-            string sql = @"Select * From DMHVCT HV Where LuongDu <> 0";
+            string sql = @"Select * From DMHVCT HV Where LuongDu <> 0 and MaCN = '" + iChiNhanh  + "'";
             DataTable dt = db.GetDataTable(sql);
             this.Close();
             foreach (DataRow row in dt.Rows)
